235bca7f1ebc9816320dd5aac63a89cd5f815c78
Robin H. Johnson <robbat2@orbis-terrarum.net>
2014-11-13 01:13:27 -0800

    Fixup.
    Signed-off-by: Robin H. Johnson <robbat2@orbis-terrarum.net>

--- manifests/install/deps.pp
+++ manifests/install/deps.pp
@@ -1,4 +1,9 @@
-class buildbot::install::deps {
+class buildbot::install::deps(
+	$enabled = true,
+	$ensure = 'present'
+) {
   include buildbot::params
-  ensure_packages($buildbot::params::package_deps)
+  if($enabled) {
+	ensure_packages($buildbot::params::package_deps, { ensure => $ensure, })
+  }
 }
